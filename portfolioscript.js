949ac6a57f028ae866ebd8c5fba8512c40f07706
samrudhipuranik16 <sam.puranik2018@gmail.com>
2025-03-08 15:52:52 +0530

    add

--- portfolioscript.js
+++ portfolioscript.js
@@ -15,3 +15,17 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 500); // Change after fade-out
     }, 1500);
 });
+document.getElementById("gitbttn1").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/project1onlinestore/index.html";
+};
+document.getElementById("gitbttn2").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/project2mybank/index.html";
+};
+document.getElementById("gitbttn3").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/mystoreproject3/index.html";
+};
+document.getElementById("gitbttn4").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/project4website/index.html";
+};
+
+// https://samrudhipuranik16.github.io/
