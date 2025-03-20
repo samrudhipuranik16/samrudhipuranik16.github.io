61a032c23849997730438d18da68269aea2c6e30
samrudhipuranik16 <sam.puranik2018@gmail.com>
2025-03-20 09:55:07 +0530

    add

--- myreactstore/react-mystorer/src/App1.js
+++ myreactstore/react-mystorer/src/App1.js
@@ -1,7 +1,4 @@
-// in index.js give app1 instead of app and write import app1 from app1
-
 import React from 'react'
-import "./App1.css"
 export default function App1() {
     let a = 10
     let b = 20
