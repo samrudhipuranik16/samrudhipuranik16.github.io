81023f2b863cd132d34364a491afa6d6b3ea94ca
samrudhipuranik16 <sam.puranik2018@gmail.com>
2025-04-04 15:29:54 +0530

    add

--- script.js
+++ script.js
@@ -1,4 +1,37 @@
-let a=10
-let b=10
-let c=a+b
-document.write(c)
+// let a=10
+// let b=10
+// let c=a+b
+// document.write(c)
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const textElement = document.querySelector("#changingText .word");
+    if (!textElement) return;
+
+    const words = ["Student", "Developer"];
+    let index = 0;
+
+    setInterval(() => {
+        textElement.classList.add("hidden"); // Fade out
+
+        setTimeout(() => {
+            index = (index + 1) % words.length;
+            textElement.textContent = words[index]; // Change text
+            textElement.classList.remove("hidden"); // Fade in
+        }, 500); // Change after fade-out
+    }, 1500);
+});
+document.getElementById("gitbttn1").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/samrudhipuranik16.github.io/project1onlinestore/index.html";
+};
+document.getElementById("gitbttn2").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/project2mybank/index.html";
+};
+document.getElementById("gitbttn3").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/mystoreproject3/index.html";
+};
+document.getElementById("gitbttn4").onclick = function() {
+    window.location.href = "https://samrudhipuranik16.github.io/project4website/index.html";
+};
+
+// https://samrudhipuranik16.github.io/
